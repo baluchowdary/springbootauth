9f4def41b41cd2e153bae855ff5d29af40114fa0
baluchowdary <bhardwaja.kollu@gmail.com>
2026-01-28 03:00:32 -0600

    CORS issue

--- src/main/java/com/kollu/angularone/controller/UserModelController.java
+++ src/main/java/com/kollu/angularone/controller/UserModelController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.kollu.angularone.model.UserModel;
 import com.kollu.angularone.service.JwtService;
 import com.kollu.angularone.service.UserModelService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/users")
 public class UserModelController {
